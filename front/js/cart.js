962f6a4b75f719d4a707a742997dc264dcb3d674
alexis77360 <84974466+alexis77360@users.noreply.github.com>
2022-09-20 18:02:27 +0200

    verification test Btn supprimer

---
+++ front/js/cart.js
@@ -0,0 +1,138 @@
+//! Fonction pour récupérer le panier
+function getCart() {
+    //? Opérateur de coalescence ( si null/undefined renvoie la 2eme valeur)
+    return JSON.parse(localStorage.getItem("monPanier")) ?? [];
+}
+
+//! Fonction pour supprimer un article
+function removeItem(e) {
+
+  let cart = getCart();
+  
+  let article = e.target.closest("article");
+
+  let newCart = cart.filter(cartItem => cartItem.idProduct !== article.dataset.id || cartItem.colorProduct !== article.dataset.color )
+
+  localStorage.setItem("monPanier", JSON.stringify(newCart));
+
+  window.location.reload()
+
+
+
+
+
+  
+}
+
+//! Fonction pour changer la quantité d'un article
+function changeQuantity(e) {
+
+    let input = e.target;
+    let article = input.closest("article");
+    let cart = getCart();
+
+    const found = cart.find(cartItem => cartItem.idProduct === article.dataset.id && cartItem.colorProduct === article.dataset.color);
+    found.qtyProduct = parseInt(input.value);
+
+    localStorage.setItem("monPanier", JSON.stringify(cart));
+
+    window.location.reload()
+
+
+  }
+
+
+
+
+//! Sélection HTML du cart
+const cartPrice = document.getElementsByClassName("cart__price");
+const cartSection = document.getElementById("cart__items");
+const cartOrder = document.getElementsByClassName("cart__order");
+const h1 = document.getElementsByTagName("h1");
+
+
+//! Fonction pour afficher le panier
+function showCart(items) {
+    let qty = 0;
+    let price = 0;
+
+    console.log(items);
+    
+      for (const item of items) {
+        
+            fetch(`http://localhost:3000/api/products/${item.idProduct}`)
+                .then((response) => response.json())
+                .then((data) => {
+
+                    cartSection.innerHTML += `<article class="cart__item" data-id="${item.idProduct}" data-color="${item.colorProduct}">
+                <div class="cart__item__img">
+                  <img src="${data.imageUrl}" alt="${data.altTxt}">
+                </div>
+                <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                    <h2>${data.name}</h2>
+                    <p>${item.colorProduct}</p>
+                    <p>${data.price} €</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Qté : </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.qtyProduct}">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                    <p class="deleteItem">Supprimer</p>
+                    </div>
+                  </div>
+                </div>
+              </article>`;
+                    //! Calcul du prix total
+                    price += data.price * item.qtyProduct;
+                    document.getElementById("totalPrice").innerHTML = price;
+                });
+
+            //! Calcul de la quantité totale
+            qty += parseInt(item.qtyProduct);
+            document.getElementById("totalQuantity").innerHTML = qty;
+        }
+      
+}
+      let cart = getCart();
+
+      if(cart.length){
+        
+        showCart(cart);
+        cartSection.onchange= changeQuantity;
+
+/*         let btns = document.getElementsByClassName('deleteItem');
+        
+        console.log(btns);
+        for (let toto of btns) {
+          
+          console.log(toto);
+          toto.onclick = removeItem;
+          
+          
+        }
+
+ */
+
+        let deleteItems = document.getElementsByClassName('deleteItem');
+  
+  for (const deleteItem of deleteItems) {
+
+    console.log("Hello");
+    deleteItem.onclick = removeItem;    
+  } 
+
+
+
+        
+
+        
+      }
+    else {
+      //! Si le panier est vide alors afficher le message
+        h1.innerHTML = `Votre panier est actuellement vide`;
+        cartOrder.innerHTML = "0";
+        cartPrice.innerHTML = "0";
+    }
--- front/js/product.js
+++ front/js/product.js
@@ -1,32 +1,95 @@
+//! Requete d'un produit
+function getProduct(id) {
+
+    return fetch(`http://localhost:3000/api/products/${id}`)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`HTTP ${res.status} - ${res.statusText}`);
+            }
+            return res.json();
+        })
+
+}
+
+
+// !!création du DOM pour la page produit
+let cardCreation = function (product) {
+
+
+    let img = document.querySelector(".item__img");
+    img.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+
+    let title = document.querySelector("#title");
+    title.innerText = product.name;
+
+    let price = document.querySelector("#price");
+    price.innerText = product.price;
+
+    let description = document.querySelector("#description");
+    description.innerText = product.description;
+
+    let select = document.querySelector("#colors");
+
+    //! boucle sur tableau avec valeur  
+    for (const color of product.colors) {
+        /*select.innerHTML += `<option value="${color}">${color}</option>`; */
+        let option = document.createElement("option");
+        option.text = color;
+        select.add(option);
+    }
+};
+
+//! Appel de l'api + appel création dom*/
+
 let url = new URL(window.location.href);
 let id = url.searchParams.get("id");
-/* console.log(id);
- */
 
-// !! Requete vers Api + création du DOM pour la page produit
-let cardCreation = function () {
-    fetch(`http://localhost:3000/api/products/${id}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
+getProduct(id)
+    .then(product => {
+        cardCreation(product);
+        document.querySelector("#addToCart").onclick = addProduct(product);
+    })
+    .catch(error => console.log(error));
 
-            let img = document.querySelector(".item__img");
-            img.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
 
-            let title = document.querySelector("#title");
-            title.innerHTML = data.name;
+//! Ajout Produit au panier
+function addProduct(product) {
 
-            let price = document.querySelector("#price");
-            price.innerHTML = data.price;
+    return () => {
 
-            let description = document.querySelector("#description");
-            description.innerHTML = data.description;
+        let qtyProduct = parseInt(document.querySelector("#quantity").value);
+        let colorProduct = document.querySelector("#colors").value;
+        let idProduct = id;
 
-            for (let i = 0; i < data.colors.length; i++) {
-                let colors = document.querySelector("#colors");
-                colors.innerHTML += `<option value="${data.colors[i]}">${data.colors[i]}</option>`;
+        if (qtyProduct < 1 || qtyProduct > 100 || colorProduct === undefined) {
+            alert("Veuillez choisir une couleur et une quantité");
+        } else {
+
+            let cart = getCart();
+
+            /* //! Renvoi l'élément trouvé */
+            const found = cart.find(cartItem => cartItem.idProduct === idProduct && cartItem.colorProduct === colorProduct);
+
+            if (found == undefined) {
+                cart.push({
+                    idProduct,
+                    colorProduct,
+                    qtyProduct
+                });
+            } else {
+                found.qtyProduct += qtyProduct;
+
             }
+            localStorage.setItem("monPanier", JSON.stringify(cart));
+            document.location.href = "./cart.html";
 
-})};
+        }
+    }
+}
 
-cardCreation();
+
+//! Affichage du panier dans le local storage
+function getCart() {
+    //? Opérateur de coalescence ( si null/undefined renvoie la 2eme valeur)
+    return JSON.parse(localStorage.getItem("monPanier")) ?? [];
+}
